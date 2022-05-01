946a54043762fbe34456a6cdb65c648697e4f6fa
rybakN <dnb4949@gmail.com>
2022-05-01 13:31:12 +0300

    feat: task season

--- src/what-season.js
+++ src/what-season.js
@@ -12,47 +12,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  let month = date.getMonth();
-
-  switch (month) {
-    case 0:
-      month = 'winter';
-      break;
-    case 1:
-      month = 'winter';
-      break;
-    case 11:
-      month = 'winter';
-      break;
-    case 2:
-      month = 'spring';
-      break;
-    case 3:
-      month = 'spring';
-      break;
-    case 4:
-      month = 'spring';
-      break;
-    case 5:
-      month = 'summer';
-      break;
-    case 6:
-      month = 'summer'
-      break;
-    case 7:
-      month = 'summer'
-      break;
-    case 8:
-      month = 'autumn'
-      break;
-    case 9:
-      month = 'autumn'
-      break;
-    case 10:
-      month = 'autumn'
-      break;
-  }
-  return month;
+  let message = 'Unable to determine the time of year!';
+  if (date instanceof Date && !date.hasOwnProperty('getMonth')) {
+    let month = date.getMonth();
+    switch (month) {
+      case 0:
+        month = 'winter';
+        break;
+      case 1:
+        month = 'winter';
+        break;
+      case 11:
+        month = 'winter';
+        break;
+      case 2:
+        month = 'spring';
+        break;
+      case 3:
+        month = 'spring';
+        break;
+      case 4:
+        month = 'spring';
+        break;
+      case 5:
+        month = 'summer';
+        break;
+      case 6:
+        month = 'summer'
+        break;
+      case 7:
+        month = 'summer'
+        break;
+      case 8:
+        month = 'autumn'
+        break;
+      case 9:
+        month = 'autumn'
+        break;
+      case 10:
+        month = 'autumn'
+        break;
+    }
+    return month;
+  } else if (arguments.length === 0) {
+    return message;
+  } else { throw new Error("Invalid date!"); }
 }
 
 module.exports = {
